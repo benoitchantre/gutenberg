a858025b3ba48a2d7dc56b13c25fe1837004d0a8
Andrei Draganescu <andrei.draganescu@automattic.com>
2023-11-23 17:34:33 +0200

    add REST class for render blocks endpoint

--- lib/compat/wordpress-6.5/rest-api.php
+++ lib/compat/wordpress-6.5/rest-api.php
@@ -19,3 +19,13 @@ function gutenberg_register_global_styles_revisions_endpoints() {
 }
 
 add_action( 'rest_api_init', 'gutenberg_register_global_styles_revisions_endpoints' );
+
+/**
+ * Registers the Block Rederer REST API routes.
+ */
+function gutenberg_register_block_rederer_routes() {
+	$block_renderer_controller = new Gutenberg_Render_Blocks_Controller();
+	$block_renderer_controller->register_routes();
+}
+
+add_action( 'rest_api_init', 'gutenberg_register_block_rederer_routes' );
